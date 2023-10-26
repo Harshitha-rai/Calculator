a273f85e4755505237021551787589621762ee87
Your Name01~ <you@example.com>
2023-10-26 17:04:22 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,27 @@
+let screen = document.getElementById("inputBox");
+let buttons = document.querySelectorAll("button");
+
+let string = "";
+
+let array = Array.from(buttons);
+
+array.forEach((button) => {
+    button.addEventListener("click", (e) => {
+        if(e.target.innerHTML == '=') {
+            string = eval(string);
+            screen.value = string;
+        }
+        else if (e.target.innerHTML == 'AC') {
+            string = "";
+            screen.value = string;
+        }
+        else if (e.target.innerHTML == 'DEL') {
+            string = string.substring(0, string.length-1);
+            screen.value = string;
+        }
+        else {
+            string += e.target.innerHTML;
+            screen.value = string;
+        }
+    })
+})
